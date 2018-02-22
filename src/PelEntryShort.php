6090664fdfebb52527dcc8d7c8a65d78016ba94f
mondrake <mondrake@mondrake.org>
2018-02-22 13:17:34 +0100

    20

--- src/PelEntryShort.php
+++ src/PelEntryShort.php
@@ -94,14 +94,15 @@ class PelEntryShort extends PelEntryNumber
         $this->max = 65535;
         $this->format = PelFormat::SHORT;
 
+        $v = [];
         if (is_null($value)) {
-            $value = func_get_args();
-            array_shift($value);
+            $v = func_get_args();
+            array_shift($v);
         } elseif (is_scalar($value)) {
-            $value[0] = $value;
+            $v[] = $value;
         }
 
-        $this->setValueArray($value);
+        $this->setValueArray($v);
     }
 
     /**
