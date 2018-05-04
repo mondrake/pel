d66564548f6735e0be16f90787ad2361c4e04bd5
mondrake <mondrake@mondrake.org>
2018-05-04 22:21:37 +0200

    132

--- src/ElementBase.php
+++ src/ElementBase.php
@@ -129,7 +129,7 @@ abstract class ElementBase implements ElementInterface, LoggerInterface
      */
     public function getPath()
     {
-        return $this->xxgetDom()->getNodePath();
+        return $this->xxgetDom() ? $this->xxgetDom()->getNodePath() : null;
         //return $this->getParentElement() ? $this->getParentElement()->getPath() . '/' . $this->getElementPathFragment() : $this->getElementPathFragment();
     }
 
