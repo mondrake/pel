28fc687e3c1e8ad11d5df79506d58a8e92504e3e
mondrake <mondrake@mondrake.org>
2018-05-20 21:15:46 +0200

    4

--- src/Utility/DumpLogFormatter.php
+++ src/Utility/DumpLogFormatter.php
@@ -9,7 +9,7 @@ use Bramus\Monolog\Formatter\ColoredLineFormatter;
  */
 class DumpLogFormatter extends ColoredLineFormatter
 {
-    const MAX_PATH = 40;
+    const MAX_PATH = 50;
 
     /**
      * {@inheritdoc}
@@ -22,7 +22,7 @@ class DumpLogFormatter extends ColoredLineFormatter
         // Path.
         if (isset($record['context']['path'])) {
             $path = $record['context']['path'];
-            $nesting = count(explode('/', $path));
+            $nesting = count(explode('/', $path)) - 1;
             if (strlen($path) < static::MAX_PATH) {
                 $path = str_pad($path, static::MAX_PATH, ' ', STR_PAD_LEFT);
             }
@@ -36,7 +36,7 @@ class DumpLogFormatter extends ColoredLineFormatter
         }
 
         // Message.
-        $output .= substr(str_repeat('  ', $nesting) . $record['message'], 0, static::MAX_PATH * 2);
+        $output .= substr(str_repeat(' ', $nesting) . $record['message'], 0, static::MAX_PATH * 2);
 
         return $this->getColorScheme()->getColorizeString($record['level']). $output . $this->getColorScheme()->getResetString() . "\n";
     }
