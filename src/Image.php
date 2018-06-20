e03467b39e788acd9c12387ddb7e7acf9639cc01
mondrake <mondrake@mondrake.org>
2018-06-20 10:45:04 +0200

    4

--- src/Image.php
+++ src/Image.php
@@ -67,7 +67,7 @@ class Image
         $data_window = new DataWindow(file_get_contents($path));
         
         // Is file a JPEG image?
-        if ($d->getBytes(0, 2) === '0xFFD8') {
+        if ($data_window->getBytes(0, 2) === '0xFFD8') {
             return new static('image/jpeg', $data_window);
         }
         
