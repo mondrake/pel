d1692a19e62a4f6a65708dd1afd5a17861dd691c
mondrake <mondrake@mondrake.org>
2018-06-22 12:15:42 +0200

    8

--- src/Image.php
+++ src/Image.php
@@ -28,21 +28,34 @@ class Image
     /**
      * Constructs a new Image object.
      */
-    public function __construct($mime_type, DataWindow $data_window)
+    public function __construct()
     {
-        $this->mimeType = $mime_type;
-        switch ($this->mimeType) {
-            case 'image/jpeg':
-                $this->root = new Jpeg();
-                $this->root->loadFromData($data_window);
-                return;
-            case 'image/tiff':
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadFromData(DataWindow $data_window, $offset = 0, array $options = [])
+    {
+        // JPEG image?
+        if ($data_window->getBytes(0, 3) === "\xFF\xD8\xFF") {
+            $this->mimeType = 'image/jpeg';
+            $this->root = new Jpeg();
+            $this->root->loadFromData($data_window);
+        }
+
+        // TIFF image?
+        $byte_order = $data_window->getBytes(0, 2);
+        if ($byte_order === 'II' || $byte_order === 'MM') {
+            $data_window->setByteOrder($byte_order === 'II' ? ConvertBytes::LITTLE_ENDIAN : ConvertBytes::BIG_ENDIAN);
+            if ($data_window->getShort(2) === Tiff::TIFF_HEADER) {
+                $this->mimeType = 'image/tiff';
                 $this->root = new Tiff();
                 $this->root->loadFromData($data_window);
-                return;
-            default:
-                throw new ExifEyeException('Unrecognized image format.');
+            }
         }
+
+        throw new ExifEyeException('Unrecognized image format.');
     }
 
     public function getMimeType()
@@ -74,9 +87,11 @@ class Image
     {
         $magic_file_info = new DataWindow(file_get_contents($path, FALSE, NULL, 0, 10));
 
+        $recognized_format = false;
+
         // JPEG image?
         if ($magic_file_info->getBytes(0, 3) === "\xFF\xD8\xFF") {
-            return new static('image/jpeg', new DataWindow(file_get_contents($path)));
+            $recognized_format = true;
         }
 
         // TIFF image?
@@ -84,30 +99,24 @@ class Image
         if ($byte_order === 'II' || $byte_order === 'MM') {
             $magic_file_info->setByteOrder($byte_order === 'II' ? ConvertBytes::LITTLE_ENDIAN : ConvertBytes::BIG_ENDIAN);
             if ($magic_file_info->getShort(2) === Tiff::TIFF_HEADER) {
-                return new static('image/tiff', new DataWindow(file_get_contents($path)));
+                $recognized_format = true;
             }
+        }
+
+        if ($recognized_format) {
+            $image = new static();
+            $image->loadFromData(new DataWindow(file_get_contents($path)));
+            return $image;
         }
 
         throw new ExifEyeException('Unrecognized image format.');
     }
 
-    public static function loadFromData(DataWindow $data_window)
+    public static function createFromData(DataWindow $data_window)
     {
-        // JPEG image?
-        if ($data_window->getBytes(0, 3) === "\xFF\xD8\xFF") {
-            return new static('image/jpeg', $data_window);
-        }
-
-        // TIFF image?
-        $byte_order = $data_window->getBytes(0, 2);
-        if ($byte_order === 'II' || $byte_order === 'MM') {
-            $data_window->setByteOrder($byte_order === 'II' ? ConvertBytes::LITTLE_ENDIAN : ConvertBytes::BIG_ENDIAN);
-            if ($data_window->getShort(2) === Tiff::TIFF_HEADER) {
-                return new static('image/tiff', $data_window);
-            }
-        }
-
-        throw new ExifEyeException('Unrecognized image format.');
+        $image = new static();
+        $image->loadFromData($data_window);
+        return $image;
     }
 
     /**
