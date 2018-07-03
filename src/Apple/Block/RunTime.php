9ac9a7f7bacc9883b5b523f9c4d0083065cc7a27
mondrake <mondrake@mondrake.org>
2018-07-03 17:28:44 +0200

    7

--- src/Apple/Block/RunTime.php
+++ src/Apple/Block/RunTime.php
@@ -13,74 +13,20 @@ class RunTime extends Ifd
      */
     public function loadFromData(DataWindow $data_window, $offset = 0, array $options = [])
     {
+        $this->debug("START... Loading");
+
         $plist = new CFPropertyList();
         $plist->parse($data_window->getBytes($options['data_offset'], $options['components']));
         dump($plist->toArray());
-    }
-
-    /**
-     * xx
-     * @param DataWindow $data_window
-     *            the data from which the thumbnail will be
-     *            extracted.
-     */
-    public static function runTimetoBlock(DataWindow $data_window, Ifd $ifd)
-    {
-        $apple_run_time_entry = $ifd->getElement("tag[@name='RunTime']/entry");
-
-        if ($apple_run_time_entry === null) {
-            return;
+        
+        // Build a TAG object for each PList item.
+        foreach ($plist->toArray() as $tag_name => $value) {
+            $tag_id = Spec::getTagIdByName($this, $tag_name);
+            $tag_format = Spec::getTagFormat($this, $tag_id);
+            $tag_entry_class = Spec::getEntryClass($this, $tag_id, $tag_format);
+            $tag = new Tag($this, $tag_id, $tag_entry_class, [$value], $tag_format, 1);
         }
 
-        $plist = new CFPropertyList();
-        $plist->parse($apple_run_time_entry->getValue());
-dump($plist->toArray());
-return;
-        $length = $ifd->getElement("tag[@name='ThumbnailLength']/entry")->getValue();
-
-        // Load the thumbnail only if both the offset and the length are
-        // available and positive.
-        if ($offset <= 0 || $length <= 0) {
-            $ifd->warning('Invalid JPEG thumbnail for offset {offset} and length {length}', [
-                'offset' => $offset,
-                'length' => $length,
-            ]);
-            return;
-        }
-
-        // Some images have a broken length, so we try to carefully check
-        // the length before we store the thumbnail.
-        if ($offset + $length > $data_window->getSize()) {
-            $ifd->warning('Thumbnail length {length} bytes adjusted to {adjusted_length} bytes.', [
-                'length' => $length,
-                'adjusted_length' => $data_window->getSize() - $offset,
-            ]);
-            $length = $data_window->getSize() - $offset;
-        }
-
-        // Now set the thumbnail normally.
-        try {
-            $dataxx = $data_window->getClone($offset, $length);
-            $size = $dataxx->getSize();
-            // Now move backwards until we find the EOI JPEG marker.
-            while ($dataxx->getByte($size - 2) != 0xFF || $dataxx->getByte($size - 1) != JpegMarker::EOI) {
-                $size --;
-            }
-            if ($size != $dataxx->getSize()) {
-                $ifd->warning('Decrementing thumbnail size to {size} bytes', [
-                    'size' => $size,
-                ]);
-            }
-            $thumbnail_data = $dataxx->getClone(0, $size)->getBytes();
-
-            $thumbnail_block = new static($ifd);
-            $thumbnail_entry = new Undefined($thumbnail_block, [$thumbnail_data]);
-            $thumbnail_block->debug('JPEG thumbnail found at offset {offset} of length {length}', [
-                'offset' => $offset,
-                'length' => $length,
-            ]);
-        } catch (DataWindowException $e) {
-            $ifd->error($e->getMessage());
-        }
+        $this->debug(".....END Loading");
     }
 }
