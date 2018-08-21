4c1d554a58b92a20bff76afa38376a6f4d3108f3
mondrake <mondrake@mondrake.org>
2018-08-21 18:17:11 +0200

    17

--- test/GH21Test.php
+++ test/GH21Test.php
@@ -55,7 +55,7 @@ class GH21Test extends ExifEyeTestCaseBase
         $out_image = Image::createFromData(new DataWindow($scaled));
         $out_jpeg = $out_image->getElement("jpeg");
 
-        $exif = $input_jpeg->getElement("segment/exif");
+        $exif = $input_jpeg->getElement("jpegSegment/exif");
 
         // Find the COM segment in the output file.
         $out_com_segment = $out_jpeg->getElement("segment[@name='COM']");
@@ -68,10 +68,10 @@ class GH21Test extends ExifEyeTestCaseBase
         $exif_block->loadFromData(new DataWindow($exif->toBytes()));
 
         $out_image->saveToFile($this->file);
-        
+
         $image = Image::loadFromFile($this->file);
         $jpeg = $image->getElement("jpeg");
-        $exifin = $jpeg->getElement("segment/exif");
+        $exifin = $jpeg->getElement("jpegSegment/exif");
         $this->assertEquals($exif, $exifin);
     }
 }
