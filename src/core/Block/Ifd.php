6b0f913f960b942f95891ea44e6a82b39770fdac
mondrake <mondrake@mondrake.org>
2018-09-02 16:09:24 +0200

    122

--- src/core/Block/Ifd.php
+++ src/core/Block/Ifd.php
@@ -90,7 +90,7 @@ class Ifd extends BlockBase
             $tag_format = $data_window->getShort($i_offset + 2);
             $tag_components = $data_window->getLong($i_offset + 4);
             $tag_data_element = $data_window->getLong($i_offset + 8);
-//dump($tag_id, $tag_format, $tag_components, $tag_data_element);
+dump($tag_id, $tag_format, $tag_components, $tag_data_element);
             // If the data size is bigger than 4 bytes, then actual data is not in
             // the TAG's data element, but at the the offset stored in the data
             // element.
