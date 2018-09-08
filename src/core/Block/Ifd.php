01d591eb832b3230f1782245c2a7ef44bf7e01c1
mondrake <mondrake@mondrake.org>
2018-09-08 17:45:11 +0200

    9

--- src/core/Block/Ifd.php
+++ src/core/Block/Ifd.php
@@ -105,6 +105,7 @@ class Ifd extends BlockBase
             }
 
             // Build the TAG object.
+dump($this->getType(), $tag_id, $tag_format);
             $tag_entry_class = Spec::getElementHandlingClass($this->getType(), $tag_id, $tag_format);
             $tag_entry_arguments = call_user_func($tag_entry_class . '::getInstanceArgumentsFromTagData', $this, $tag_format, $tag_components, $data_element, $tag_data_offset);
             $tag = new Tag('tag', $this, $tag_id, $tag_entry_class, $tag_entry_arguments, $tag_format, $tag_components);
