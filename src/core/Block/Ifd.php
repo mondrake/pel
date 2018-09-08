1b1966dd32e5a4b4e145e3c442c32f55e4cef8b8
mondrake <mondrake@mondrake.org>
2018-09-08 23:20:45 +0200

    44

--- src/core/Block/Ifd.php
+++ src/core/Block/Ifd.php
@@ -112,7 +112,6 @@ class Ifd extends BlockBase
                 $tag_entry_arguments = call_user_func($tag_entry_class . '::getInstanceArgumentsFromTagData', $this, $tag_format, $tag_components, $data_element, $tag_data_offset);
                 $tag = new Tag('tag', $this, $tag_id, $tag_entry_class, $tag_entry_arguments, $tag_format, $tag_components);
             } else {
-dump($element_type, $tag_entry_class, $this->getType(), $tag_id);
                 // If the tag is an IFD pointer, loads the IFD.
                 $ifd_type = Spec::getElementType($this->getType(), $tag_id);
                 $ifd_name = Spec::getElementName($this->getType(), $tag_id);
