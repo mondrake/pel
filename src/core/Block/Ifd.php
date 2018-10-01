96ed6fbf39cf437d8353286936d39942a811fc36
mondrake <mondrake.org@gmail.com>
2018-10-01 14:32:43 +0200

    13

--- src/core/Block/Ifd.php
+++ src/core/Block/Ifd.php
@@ -214,12 +214,12 @@ class Ifd extends BlockBase
             if ($sub_block->getType() === 'thumbnail') {
                 $thumbnail_entry = $sub_block->getElement('entry');
                 // Add length.
-                $bytes .= ConvertBytes::fromShort(Spec::getTagIdByName($this, 'ThumbnailLength'), $byte_order);
+                $bytes .= ConvertBytes::fromShort(Spec::getElementIdByName($this->getType(), 'ThumbnailLength'), $byte_order);
                 $bytes .= ConvertBytes::fromShort(Format::LONG, $byte_order);
                 $bytes .= ConvertBytes::fromLong(1, $byte_order);
                 $bytes .= ConvertBytes::fromLong($thumbnail_entry->getComponents(), $byte_order);
                 // Add offset.
-                $bytes .= ConvertBytes::fromShort(Spec::getTagIdByName($this, 'ThumbnailOffset'), $byte_order);
+                $bytes .= ConvertBytes::fromShort(Spec::getElementIdByName($this->getType(), 'ThumbnailOffset'), $byte_order);
                 $bytes .= ConvertBytes::fromShort(Format::LONG, $byte_order);
                 $bytes .= ConvertBytes::fromLong(1, $byte_order);
                 $bytes .= ConvertBytes::fromLong($data_area_offset, $byte_order);
