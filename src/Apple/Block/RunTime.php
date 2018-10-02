334c53c83aaef51995210f0fc22d5817712abb91
mondrake <mondrake.org@gmail.com>
2018-10-02 16:07:23 +0200

    Ap10

--- src/Apple/Block/RunTime.php
+++ src/Apple/Block/RunTime.php
@@ -29,6 +29,7 @@ class RunTime extends Ifd
         $this->debug("START... Loading");
 
         $plist = new CFPropertyList();
+dump(ExifEye::dumpHex($data_element->getBytes($options['data_offset'], $options['components']), 40));
         $plist->parse($data_element->getBytes($options['data_offset'], $options['components']));
         $this->pList = $plist;
 
@@ -48,6 +49,7 @@ class RunTime extends Ifd
      */
     public function toBytes($byte_order = ConvertBytes::LITTLE_ENDIAN, $offset = 0, $has_next_ifd = false)
     {
+dump(ExifEye::dumpHex($this->pList->toBinary(), 40));
 //dump($this->pList);
         return $this->pList->toBinary();
     }
